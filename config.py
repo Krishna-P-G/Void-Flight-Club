178cfdacd6b6b18b427d614f9f9dac17d6eaf2ad
Krishna.P.G <110535341+Krishna-P-G@users.noreply.github.com>
2023-08-14 16:22:58 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-SHEETY_AUTH = "Bearer REST"
-SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/d4f37a9856e1e0ee67474ea669542915/flightDeals/prices"
-SHEETY_USERS_ENDPOINT = "https://api.sheety.co/d4f37a9856e1e0ee67474ea669542915/flightDeals/users"
-
-TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = "MZIGQ0sKVL5G4uwK2QX4Rnr6464L0sur"
-
-my_email = "krishnamel39@gmail.com"
-password = "cnqtnuzviqpksnbe"
